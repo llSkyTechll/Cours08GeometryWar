67613f818d6c69dab6713622defff742faee8e2e
Alexandre Reny <areny1998@hotmail.com>
2019-09-16 17:08:26 -0400

    Premier Commit

---
+++ Geometry War/Assets/Script/Ennemi.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public abstract class Ennemi : MonoBehaviour, Damageable {
+
+    public int lifeTotal = 1;
+
+    public void TakeDamage(int damage)
+    {
+        lifeTotal -= damage;
+        if (lifeTotal <= 0)
+        {
+            Die();
+        }
+    }
+
+    public abstract void Die();
+
+    protected void SetLife(int newLifeTotal)
+    {
+        lifeTotal = newLifeTotal;
+    }
+}
